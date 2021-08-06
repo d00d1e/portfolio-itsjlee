4ea166b530746e99020a2130d16d9405429a5d50
d00d1e <lee.julianay@gmail.com>
2021-08-06 11:02:16 -0700

    fix mobile view background image zoom

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -9,16 +9,11 @@ export default function ProjectCard({ projects }) {
   const [filter, setFilter] = useState("all");
 
   // This will filter projects based on Category
-  const filteredProjects = projects.filter((project) =>
-    project.category.includes(filter)
-  );
+  const filteredProjects = projects.filter((project) => project.category.includes(filter));
 
   return (
     <section id="project-card">
-      <div
-        id="project-card-filters"
-        className="d-flex flex-row align-items-center w-25 pb-3 pt-4"
-      >
+      <div id="project-card-filters" className="d-flex flex-row align-items-center w-25 pb-3 pt-4">
         {filters.map((filter) => (
           <button
             className="filter-buttons text-uppercase mx-auto p-3 mr-3"
@@ -36,17 +31,12 @@ export default function ProjectCard({ projects }) {
               <ul className="col-md-6 col-xl-4">
                 <li className="card-list">
                   <div className="card">
-                    {project.featured && (
-                      <img className="featured" src={img} alt="featured" />
-                    )}
+                    {project.featured && <img className="featured" src={img} alt="featured" />}
                     <Link to={`/projects/${project._id}`}>
                       <img
                         rel="preload"
                         as="image"
-                        src={
-                          require(`../assets/img/projects/${project.imageUrl}`)
-                            .default
-                        }
+                        src={require(`../assets/img/projects/${project.imageUrl}`).default}
                         alt={project.title}
                         className="card-img-top"
                         width="384px"
@@ -55,9 +45,7 @@ export default function ProjectCard({ projects }) {
                     </Link>
                     <div className="card-body m-0 pt-2">
                       <div>
-                        <p className="card-title text-uppercase h5 m-0">
-                          {project.title}
-                        </p>
+                        <p className="card-title text-uppercase h5 m-0">{project.title}</p>
                         <p className="text-muted h6 m-0">{project.caption}</p>
                       </div>
                     </div>
